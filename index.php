e9d7f195cedb294e0d9538a0c22bde3bac3ec09e
Pratyashshrivastava <kush.shrivastava12@gmail.com>
2023-09-09 09:52:45 +0530

    completed the update function

--- index.php
+++ index.php
@@ -15,20 +15,38 @@ $insert = false;
     die("Failed to Connect: ". mysqli_connect_error());
   }
   if ($_SERVER['REQUEST_METHOD'] == "POST"){
-    $title = $_POST['title'];
-    $description = $_POST['description'];
+  if(isset($_POST['snoEdit'])){
+    // Update the record
+      $sno = $_POST['snoEdit'];
+      $title = $_POST['titleEdit'];
+      $description = $_POST['descriptionEdit'];
 
-  // Sql query to be executed
-  $sql = "INSERT INTO `notes`.`notes`(`title`, `description`) VALUES('$title', '$description')";
-  $result = mysqli_query($con, $sql);
-
-  // checking if the addition is successfull or not
-  if($result){
-    // echo "Record has been successfully added<br>";
-    $insert = true;
+    // Sql query to be executed
+    $sql = "UPDATE `notes`.`notes` SET `title` = '$title' , `description` = '$description' WHERE `notes`.`sno` = $sno";
+    $result = mysqli_query($con, $sql);
+    // if($result){
+    //   echo "We updated the record successfully";
+    // }
+    // else{
+    //   "We could not update the record successfully";
+    // }
   }
   else{
-    echo "Encountered an Error ". mysqli_connect_error($con);
+      $title = $_POST['title'];
+      $description = $_POST['description'];
+
+    // Sql query to be executed
+    $sql = "INSERT INTO `notes`.`notes`(`title`, `description`) VALUES('$title', '$description')";
+    $result = mysqli_query($con, $sql);
+
+    // checking if the addition is successfull or not
+    if($result){
+      // echo "Record has been successfully added<br>";
+      $insert = true;
+    }
+    else{
+      echo "Encountered an Error ". mysqli_connect_error($con);
+    }
   }
   }
 ?>
@@ -144,7 +162,7 @@ $insert = false;
   ?>
   <div class="container my-5">
     <h2>Add a Note</h2>
-    <form action="/php-CRUD-tutorial/index.php" , method="post">
+    <form action="/php-CRUD-tutorial/index.php"  method="post">
       <div class="mb-3">
         <label for="title" class="form-label">Note Title</label>
         <input type="text" class="form-control" id="title" name="title" aria-describedby="emailHelp">
